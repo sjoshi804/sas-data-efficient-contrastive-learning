537d77bc46a960b160e28ca5853f3ad7c90b200a
Siddharth Joshi <sjoshi804@gmail.com>
2023-04-27 14:38:10 -0700

    Basic subset methdods added

---
+++ src/base_subset_dataset.py
@@ -0,0 +1,40 @@
+from abc import ABC
+from torch.utils.data import Dataset
+
+class SubsetDataset(ABC, Dataset):
+    def __init__(
+        self,
+        dataset: Dataset,
+        subset_fraction: float,
+        verbose: bool = False
+    ):
+        """
+        :param dataset: Original Dataset
+        :type dataset: Dataset
+        :param subset_fraction: Fractional size of subset
+        :type subset_fraction: float
+        :param verbose: verbose
+        :type verbose: boolean
+        """
+        self.dataset = dataset
+        self.subset_fraction = subset_fraction
+        self.len_dataset = len(self.dataset)
+        self.subset_size = int(self.len_dataset * self.subset_fraction)
+        self.subset_indices = None
+        self.verbose = verbose 
+
+        if verbose:
+            print(f"Subset Size: {self.subset_size}")
+            print(f"Discarding {self.len_dataset - self.subset_size} examples...")
+
+    def __len__(self):
+        return self.subset_size
+    
+    def __getitem__(self, index):
+        # Get the index for the corresponding item in the original dataset
+        original_index = self.subset_indices[index]
+        
+        # Get the item from the original dataset at the corresponding index
+        original_item = self.dataset[original_index]
+        
+        return original_item
